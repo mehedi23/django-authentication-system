c35fe62036016380f3e37479f9ac30b1e822ac66
anmamun0 <anmamun0@gmail.com>
2025-11-02 13:16:33 +0600

    chore(celery): initial Celery setup
    Created celery.py and updated core/__init__.py to initialize Celery in the project. Configured basic settings without connecting to a broker yet.

---
+++ core/celery.py
@@ -0,0 +1,18 @@
+import os
+from celery import Celery
+
+# Set default Django settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+app = Celery('core')
+
+# Use Django settings with 'CELERY_' namespace
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Auto-discover tasks from installed apps
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
--- core/settings.py
+++ core/settings.py
@@ -172,3 +172,8 @@ SIMPLE_JWT = {
     'USER_ID_FIELD': "username",
     'USER_ID_CLAIM': "username",
 }
+
+# settings.py
+# Run windows Terminal: celery -A core worker --loglevel=INFO -P solo
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
